2c145204c22508af21adf3be4478092b7df1507c
joaotuneli56 <joaopedromouratuneli@gmail.com>
2024-08-15 22:31:26 -0300

    mensagem

--- src/routes/index.js
+++ src/routes/index.js
@@ -57,4 +57,23 @@ router.post('/submit', (req, res) => {
     res.status(201).send('Dado criado com sucesso!');
 })
 
+//status 404
+let items=[
+    {id: 1, nome: "item1"},
+    {id: 2, nome: "item2"},
+    {id: 3, nome: "item3"},
+]
+
+router.get("/items/:id", (req,res) => {
+    const id = parseInt (req.params.id)
+
+    const item = items.find(item=> item.id == id)
+
+    if(item){
+        return res.status(200).send(item);
+    } else {
+        return res.status(404).send("Item não encontrado");
+    }
+})
+
 module.exports = router
